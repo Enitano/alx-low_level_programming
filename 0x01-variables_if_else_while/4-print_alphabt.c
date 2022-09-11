288b4d2501b0bd484c47c1119408051b06b22429
Enitan Osunsanya <e.osunsanya@gmail.com>
2022-09-11 13:27:35 -0700

    All alphabets except q&e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - print all alphabets
+ *except q & e
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char A;
+	
+	for (A = 'a'; A <= 'z'; A++)
+	{
+		if(A = 'e')
+			putchar(A++);
+		if(A = 'q')
+			putchar(A++);
+	}
+		putchar(A);
+		putchar('\n');
+
+	return (0);
+
+}
