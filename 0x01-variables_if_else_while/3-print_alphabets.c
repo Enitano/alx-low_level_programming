76ba982400cf4611ce79690c5446be4d79fec62c
Enitan Osunsanya <e.osunsanya@gmail.com>
2022-09-11 01:08:11 -0700

    Lower&Uppercase letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - Print lowercase and uppercase alphabets
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (A = 'A'; A <= 'Z'; A++)
+	{
+		putchar(A);
+		putchar(tolower(A));
+
+	}
+	putchar('\n');
+	return (0);
+
+}
