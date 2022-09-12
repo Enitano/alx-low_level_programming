db0b878a3e1720955d6af6e80747548928ac4ec4
Enitan Osunsanya <e.osunsanya@gmail.com>
2022-09-11 17:23:05 -0700

    0to9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - print single numbers
+ *from 0-9
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int A;
+
+	for (A = 0; A < 10; A++)
+	{
+		putchar(A);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
+
