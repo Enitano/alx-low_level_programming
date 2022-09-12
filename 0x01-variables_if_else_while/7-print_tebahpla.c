03866f196b595af8f7d6f4d44e470b7fc8c27e46
Enitan Osunsanya <e.osunsanya@gmail.com>
2022-09-11 20:03:35 -0700

    Reverse alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - Print the alphabet in reverse
+ *
+ *Return: Always 0 (Success)
+ */
+
+intd main(void)
+{
+	char A;
+
+	for (A ='z'; A >= 'a'; A--)
+	{
+		putchar(A);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
