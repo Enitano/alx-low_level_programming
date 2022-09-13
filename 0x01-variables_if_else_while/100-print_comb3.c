3c91b463e181c50f9c0137a3790e5dad8f07627d
Enitan Osunsanya <e.osunsanya@gmail.com>
2022-09-12 20:52:41 -0700

    Combine 2 digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - print possible combination
+ *of 2 numbers
+ *Return: Always 0 (0)
+ */
+
+int main(void)
+{
+	int A, B;
+
+	for (A = 48; A <= 58; A++)
+	{
+		for (B = 48; B <= 58; B++)
+		{
+			putchar("%d %d\n" A, B);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
