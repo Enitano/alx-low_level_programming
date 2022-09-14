dd4f161005109b4448c82e8692b791ac5df6ab9b
Enitan Osunsanya <e.osunsanya@gmail.com>
2022-09-13 23:23:50 -0700

    Alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 
 /**
  *main - Print alphabet() in lower case
- *void print_alphabet(void)
+ *print_alphabet - prints the alphabet, in lowercase, followed by a new line.
  *Return: Always 0 (Success)
  */
 
