2e4ac54b347f2f5b9f4676ac0093e9abb49fe254
Enitan Osunsanya <e.osunsanya@gmail.com>
2022-09-13 20:57:01 -0700

    Main.h

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *main - Print _putchar
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int A = "_putchar";
+
+	putchar(A);
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+Learning functions and nested loops
+Using "putchar" to print results
