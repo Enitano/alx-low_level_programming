cad55db4f54f331fa9b96864a22052dc4fe77b2f
Enitan Osunsanya <e.osunsanya@gmail.com>
2022-09-14 17:15:58 -0700

    check for alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *int _isalpha(int c) - checks for alphabetic character.
+ *Returns 1 if c is a letter, lowercase or uppercase
+ *Returns 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	return ((c >='A' && c <= 'z') || (c >= 'a' && c <= 'z'));
+}
