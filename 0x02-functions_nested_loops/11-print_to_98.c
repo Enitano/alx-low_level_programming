30760c4bda9bd6ebb7c4bae998b464d514851f9e
Enitan Osunsanya <e.osunsanya@gmail.com>
2022-09-14 21:07:01 -0700

    0 to 100 real quick

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n:start number
+ */
+
+void print_to_98(int n)
+{
+	int a, b;
+
+	if (n <= 98)
+	{
+		for (a = 0; a <= 98; a++)
+		{
+			if (a != 98)
+			{
+				printf("%d, ", a);
+			}
+			else if (a == 98)
+			{
+				printf("%d\n", a);
+			}
+		}
+	else if (n >= 98)
+	{
+		for (b = 0; b >= 98; b--)
+		{
+			if (b != 98)
+			{
+				printf("%d, ", b);
+			}
+			else if (b == 98)
+			{
+				printf("%d\n", b);
+			}
+		}
+	}
+	}
+}
