7b7b6ef4271a7debfa09bc92855372d695aea2bb
Enitan Osunsanya <e.osunsanya@gmail.com>
2022-09-14 19:44:02 -0700

    Checks alphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *int _isalpha(int c) - checks for alphabetic character.
- *@c - character to check
- *Returns 1 if c is a letter, lowercase or uppercase
+ *_isalpha - checks for alphabetic character.
+ *@c: character to check
+ *Return: 1 if c is a letter, lowercase or uppercase
  *Returns 0 otherwise
  */
 
