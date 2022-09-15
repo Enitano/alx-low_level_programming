fd30ffe3aa8efc73ab53938b475b8fead315ef7a
Enitan Osunsanya <e.osunsanya@gmail.com>
2022-09-14 21:35:09 -0700

    Prints alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 
 /**
  *print_alphabet_x10
- *print_alphabet_x10() - prints 10 times the alphabet,
+ *print_alphabet - prints 10 times the alphabet,
  *in lowercase, followed by a new line.
  *
  *Return: Always 0 (Success)
