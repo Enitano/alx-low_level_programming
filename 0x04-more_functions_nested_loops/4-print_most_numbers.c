0b73699307bc72cc9babb9351fc939d2305cb097
Enitan Osunsanya <e.osunsanya@gmail.com>
2022-09-16 15:48:57 -0700

    print to 9 exempt 2 &4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9
+ * except 2 & 4
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if ((i == 5)0 || (i == 52))
+		{
+			continue;
+		}
+		_putchar(i);
+	}
+	_putchar(10);
+}
