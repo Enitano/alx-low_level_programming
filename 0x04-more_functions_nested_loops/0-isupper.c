c9a75d46cad9d6e888cda997292f88e0acaeb291
Enitan Osunsanya <e.osunsanya@gmail.com>
2022-09-16 15:10:05 -0700

    Uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,10 @@
 #include "main.h"
 
-/**
- * _isupper - checks for uppercase character
- * @c: integer to check
- * Return: 1 success 0 otherwise
- */
-
 int _isupper(int c)
 {
 	if ((c >= 'A') && (c <= 'Z'))
+	{
 		return (1);
-	else
+	}
 		return (0);
 }
