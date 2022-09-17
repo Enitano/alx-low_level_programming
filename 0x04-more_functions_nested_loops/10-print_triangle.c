17dcd43d1d50a622edfc6eb0f303fafed0896569
Enitan Osunsanya <e.osunsanya@gmail.com>
2022-09-16 20:10:22 -0700

    Triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,9 +14,9 @@ void print_triangle(int size)
 	{
 		_putchar('\n');
 	}
-	for (a = 1; a <= size; i++)
+	for (a = 1; a <= size; a++)
 	{
-		for (b = 1; b <= size; j++)
+		for (b = 1; b <= size; b++)
 		{
 			if (b <= (size - a))
 			{
