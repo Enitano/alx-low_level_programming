c8a3126949f6c291b5fedb4a05a97685b1a823aa
Enitan Osunsanya <e.osunsanya@gmail.com>
2022-09-16 18:34:24 -0700

    lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number of times _ should be printed
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	while (n-- > 0)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
