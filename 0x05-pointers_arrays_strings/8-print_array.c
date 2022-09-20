14706a8a911c830db88a2a52f893d5939b8bd2be
Enitan Osunsanya <e.osunsanya@gmail.com>
2022-09-19 22:02:58 -0700

    Arrays dont point

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @n: number of elements of array
+ * @a: pointer to array
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			_putchar(",");
+			_putchar(" ");
+		}
+	}
+	_putchar('\n');
+}
