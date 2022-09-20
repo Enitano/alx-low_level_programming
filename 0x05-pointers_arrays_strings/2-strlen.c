4d69f886ff201340ee9c94479a8f849f37464132
Enitan Osunsanya <e.osunsanya@gmail.com>
2022-09-19 19:43:34 -0700

    String length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to check
+ * Return: void
+ */
+
+int _strlen(char *s)
+{
+	int str;
+
+	str = 0;
+	while (s[str] != '\0')
+	{
+		str++
+	}
+	return (str);
+}
