66b052c7a98f4c2b98d78739db8dbfb2c4ac5e32
Enitan Osunsanya <e.osunsanya@gmail.com>
2022-09-19 21:04:49 -0700

    Reversal

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,13 +8,13 @@
 
 void print_rev(char *s)
 {
-	int i;
+	int index;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (index = 0; s[index] != '\0'; index++)
 	{
-		for (i = i - 1; s[i] != '\0'; i--)
+		for (index = index - 1; s[index] != '\0'; index--)
 		{
-			_putchar(s[i]);
+			_putchar(s[index]);
 		}
 	}
 	_putchar('\n');
