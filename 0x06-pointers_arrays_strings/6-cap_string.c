d65eec7a5af9782b66f0518aea2919764cc2d547
Enitan Osunsanya <e.osunsanya@gmail.com>
2022-09-22 09:53:22 -0700

    capslock

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,4 +2,27 @@
 
 /**
  * cap_string - function that capitalizes all words of a string
- *
+ * @str: string pointer
+ * Return: pointer
+ */
+
+char *cap_string(char *)
+{
+	int symb[14] = {' ', '\t', '\n', ',', ';', '.', '!',
+		'?', '"', '(', ')', '{', '}'};
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[0] >= 97 && str[0] <= 122)
+		{
+			str[0] = str[0] - 32;
+		}
+		for (j = 0; j < 14; j++)
+		{
+			if (str[i] >= 97 && str[i] <= 122 && str[i - 1] == symb[j])
+				str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
