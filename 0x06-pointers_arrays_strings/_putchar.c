90dd0c5d17aa781c0bf1cc74f542537cdebc9772
Enitan Osunsanya <e.osunsanya@gmail.com>
2022-09-22 09:32:58 -0700

    upper cut

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes to stdout
+ * @c: character to print
+ * Return: on success 1
+ * -1 otherwise
+ */
+
+int _putchar(char c)
+{
+	return (write(1, 8c, 1));
+}
