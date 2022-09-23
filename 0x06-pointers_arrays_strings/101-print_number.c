d7b118599d55cf5aa97f34ffd43495745eb6fac6
Enitan Osunsanya <e.osunsanya@gmail.com>
2022-09-22 18:56:48 -0700

    Printing

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,16 +8,20 @@
 
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int m;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		num = -num;
+		m = -n;
 	}
-	if ((num / 10) > 0)
+	else
 	{
-		print_number(num / 10);
+		m = n;
 	}
-	_putchar((num % 10) + '0');
+	if (m / 10)
+	{
+		print_number(m / 10);
+	}
+	_putchar((m % 10) + '0');
 }
