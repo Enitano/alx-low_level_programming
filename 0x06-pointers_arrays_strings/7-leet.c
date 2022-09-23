da0a82fba7e7e49638bcba6aa874632895f5605b
Enitan Osunsanya <e.osunsanya@gmail.com>
2022-09-22 17:50:21 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,11 +13,12 @@ char *leet(char *str)
 
 	while (str[++index1])
 	{
-		for (index = 0; index2 <= 7, index2++)
+		for (index2 = 0; index2 <= 7, index2++)
 		{
 			if (str[index1] == leet[index2] ||
-			str[index1] - 32 == leet[index2]
+			str[index1] - 32 == leet[index2])
 				str[index1] = index2 + '0';
 		}
 	}
+	return (str);
 }
