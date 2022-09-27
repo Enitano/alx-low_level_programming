c70a419bbbf1f088e13dc227c97e9a63432ab7e1
Enitan Osunsanya <e.osunsanya@gmail.com>
2022-09-26 19:53:41 -0700

    diagonals

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,17 +11,17 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, j, p, l = 0, r = 0;
+	int i, j;
+	int c = 0;
+	int d = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		p = (i * size) + i;
-		l += *(a + p);
+		c = c + *(a + i * sizeof(int));
 	}
 	for (j = 0; j < size; j++)
 	{
-		p = (j * size) + (size - 1 - j);
-		r += *(a + p);
+		d = d + *(a + (size * j) + (size - j - 1));
 	}
-	printf("%i", "%i\n", l, r);
+	printf("%d, %d\n", c, d);
 }
