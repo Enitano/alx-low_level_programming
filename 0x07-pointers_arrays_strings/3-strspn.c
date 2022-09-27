c3df1a102f65bb6c24f32472274d40e12649bc6c
Enitan Osunsanya <e.osunsanya@gmail.com>
2022-09-27 05:36:27 -0700

    test span

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,9 +1,16 @@
 #include "main.h"
 
+/**
+ * _strspn- gets the length of a prefix substring
+ * @s: string
+ * @accept: string
+ * Return: void
+ */
+
 unsigned int _strspn(char *s, char *accept)
 {
 	while (*s)
-	{ 
+	{
 		s++;
 		if (*s == accept)
 			count++;
