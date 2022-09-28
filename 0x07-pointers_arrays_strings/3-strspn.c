4b1d0c764cd9bea2a252799ac5667e418fe2b3f0
Enitan Osunsanya <e.osunsanya@gmail.com>
2022-09-27 18:14:35 -0700

    test span

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,27 +2,24 @@
 
 /**
  * _strspn - gets the length of a prefix substring
- * @s: string to segment
- * @accept: string
- * Return: s bytes on accepts
+ * @s: string segment
+ * @accept: control string
+ * Return: number of s in accept
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int count;
+	unsigned int i, j;
 
-	while (*s != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		while (*accept != '\0')
+		for (j = 0; accept[j] != s[i]; j++)
 		{
-			if (*s == *accept)
-				break;
-			count++;
-			accept++;
-			s++;
+			if(accept[j] != '\0')
+			{
+				return (i);
+			}
 		}
-		if (*accept == '\0')
-			break;
 	}
-	return (count + 1);
+	return (i);
 }
