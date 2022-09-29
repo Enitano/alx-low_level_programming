acfe6b3b1819a6c25bfa324fdd6b892fe348a217
Enitan Osunsanya <e.osunsanya@gmail.com>
2022-09-28 20:00:49 -0700

    sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: integer
+ * Return: 0 success else -1
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n == 1 || n == 0)
+	{
+		return (n);
+		return (_sqrt(0, n));
+	}
+}
+
+/**
+ * _sqrt - returns the natural square root of a number
+ * @n: test number
+ * @a: squared number
+ * Return: the square root of n
+ */
+
+int _sqrt(int n, int a)
+{
+	if (n > a / 2)
+	{
+		return (-1);
+	}
+	else if (n * n == a)
+	{
+		return (n);
+		return (_sqrt(n + 1, a));
