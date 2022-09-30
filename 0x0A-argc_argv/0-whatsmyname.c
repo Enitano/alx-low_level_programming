3c5f3efb94bd6d2f914231d88a1c8e2bc98472ea
Enitan Osunsanya <e.osunsanya@gmail.com>
2022-09-30 15:19:47 -0700

    say my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 /**
- * prog_name - prints name of program
+ * main - prints the name of the program
  * @argc: argument couunt
  * @argv: vector or array of arguments
  * Return: 0
- *i
+ */
 
-int prog_name(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 
