63a4f1e66598227fa208def00d4e61407ac9cbc5
Enitan Osunsanya <e.osunsanya@gmail.com>
2022-09-30 14:52:46 -0700

    Say my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * prog_name - prints name of program
+ * @argc: argument couunt
+ * @argv: vector or array of arguments
+ * Return: 0
+ */
+
+int prog_name(int argc, char *argv[])
+{
+	_putchar(argv[0]);
+
+	return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,5 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+_putchar(char c);
+
+int prog_name(int argc, char *argv[]);
 
 #endif /* MAIN_H */
