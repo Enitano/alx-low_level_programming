8e716487ca595233ddbe8e6e50da386fe0b68c52
Enitan Osunsanya <e.osunsanya@gmail.com>
2022-09-30 21:19:31 -0700

    static

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: size of args
+ * @argv: array of args
+ * Return: 0 success, 1 otherwise
+ */
+
+int main(int argc, char argv)
+{
+	int index, sum;
+
+	if (argc <= 1)
