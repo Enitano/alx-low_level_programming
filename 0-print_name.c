768c1a7657863bca76f24c77977886366a96a95b
Enitan Osunsanya <e.osunsanya@gmail.com>
2022-10-12 18:11:23 -0700

    Name

---
+++ 0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: given name
+ * @f: pointer to function to print name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		return;
+	f(name);
+}
